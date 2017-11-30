15137c035a9881536337702088e738b90c1f593a
Trent Holliday <tholliday@morristechnology.com>
2017-11-30 10:07:28 -0500

    Created an instance of `CollectionPermissionPolicy`.

--- wagtail/wagtailcore/permissions.py
+++ wagtail/wagtailcore/permissions.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
 
-from wagtail.wagtailcore.models import Site
+from wagtail.wagtailcore.models import Collection, Site
 from wagtail.wagtailcore.permission_policies import ModelPermissionPolicy
+from wagtail.wagtailcore.permission_policies.collections import CollectionPermissionPolicy
 
 site_permission_policy = ModelPermissionPolicy(Site)
+collection_permission_policy = CollectionPermissionPolicy(Collection)
